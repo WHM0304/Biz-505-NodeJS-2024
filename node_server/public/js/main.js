862e7f15860b7574cf0c49d0f3bda3529ca3efd8
WHM0304 <whm0304@naver.com>
2024-01-15 19:22:56 +0900

    nodejs_복습

--- node_server/public/js/main.js
+++ node_server/public/js/main.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello Korea");
   const nav_ul = document.querySelector("nav ul");
   nav_ul.addEventListener("click", (event) => {
-    // li tag
     const target = event.target;
     if (target.tagName === "LI") {
       const className = target.className;
--- node_server/routes/users.js
+++ node_server/routes/users.js
@@ -11,16 +11,21 @@ router.get("/", async (req, res, next) => {
     dept: st_dept,
     grade: st_grade,
   };
+  /**
+   * req 할내용을 st_변수 에 할당하고 그 값을 users 항목의 인풋 이름들의 변수에 할당
+   * 이후에 users에 req를 보내고 res값을 3가지 값을 출력해주기
+   */
   res.render("users", student);
-}); // router.get("/")
+});
 
 router.post("/", async (req, res) => {
-  // const name = req.body.name;
-  // const grade = req.body.grade;
-  // const dept = req.body.dept;
-  // 위의 코드를 아래의 코드와 같음
   const { name, grade, dept } = req.body;
-
+  /**
+   * 리퀘스트바디에서 받은 값을 name, grade, dept 에 할당하기
+   * 이후 res에 랜더링 하기
+   * req 를 users에 하고 name,grade, dept 값 할당받기
+   */
   res.render("users", { name, grade, dept });
 });
+
 export default router;

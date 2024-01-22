1b7eaac3fb627fbfebdc2065cee5daf551eb226a
WHM0304 <whm0304@naver.com>
2024-01-22 11:51:34 +0900

    0122

--- node_910_readbook/routes/books.js
+++ node_910_readbook/routes/books.js
@@ -1,7 +1,30 @@
 import express from "express";
+import DB from "../config/mysql.js";
+
 const router = express.Router();
+/**
+ * DB 연결을 시도하는 DB.init() 함수는 async 키워드가 부착되었다
+ * 이 함수는 동기방식으로 실행되는데
+ * 일반적인 변수 = DB.init() 방식으로 return 값을 받을수 없다
+ * DB.init() 함수의 return 값은 .then() 함수를 통해서 받아야 한다
+ */
+let dbConn = null;
+// init() 함수에 async 가 설정되어 동기식으로 작동된다
+// 이 함수의 return 값을 받기 위해서는 .then() 함수를 통하여 받아야 한다
+DB.init().then((connection) => (dbConn = connection));
 
 router.get("/", (req, res) => {
-  return res.render("books/main");
+  const sql = " SELECT * FROM tbl_books ";
+  dbConn
+    // query() 함수를 동기방식으로 진행
+    .query(sql)
+    // query() 함수 실행완료되면 .then() 함수에게 결과를 전달한다
+    .then((rows) => {
+      return res.render("books/main", { books: rows });
+    })
+    // 만약 실행중에 오류가 발생하면 .catch() 함수에게 결과를 전달한다
+    .catch((err) => {
+      return res.json(err);
+    });
 });
 export default router;

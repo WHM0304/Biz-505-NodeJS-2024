10f0e39419d587f185d0dc8dfeaf9ffb0ff47a9e
WHM0304 <whm0304@naver.com>
2024-02-05 11:50:21 +0900

    0205

--- node_030_iolist/routes/product.js
+++ node_030_iolist/routes/product.js
@@ -7,14 +7,34 @@ const IOLIST = DB.models.tbl_iolist;
 const DEPTS = DB.models.tbl_depts;
 
 router.get("/", async (req, res) => {
-  const rows = await PRODUCTS.findAll();
-  return res.render("product/list", { PRODUCTS: rows });
+  try {
+    const rows = await PRODUCTS.findAll({ limit: 10, order: [["p_code", "ASC"]] });
+    return res.render("product/list", { PRODUCTS: rows });
+    // return res.json(rows);
+  } catch (error) {
+    return res.json(error);
+  }
+
+  // return res.json(rows);
 });
 
-router.get("/:pcode/detail", async (req, res) => {
+// router.get("/:pcode/detail", async (req, res) => {
+//   const pcode = req.params.pcode;
+//   const row = await PRODUCTS.findByPk(pcode, { include: { model: IOLIST, as: "IOS", include: { model: DEPTS, as: "IO_거래처" } } });
+//   return res.render("product/detail", { PRODUCT: row });
+// });
+
+router.get("/:pcode/detail1", async (req, res) => {
   const pcode = req.params.pcode;
   const row = await PRODUCTS.findByPk(pcode, { include: { model: IOLIST, as: "IOS", include: { model: DEPTS, as: "IO_거래처" } } });
-  return res.render("product/detail", { PRODUCT: row });
+  return res.render("product/detail1", { PRODUCT: row });
+});
+
+router.get("/insert", async (req, res) => {
+  return res.render("product/input");
+});
+router.post("/insert", async (req, res) => {
+  res.json(req.body);
 });
 
 export default router;
